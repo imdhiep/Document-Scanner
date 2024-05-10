0ad4f06c50b2e7b64e89f817a202e3e083bd42f2
Dương Văn Hiệp <129377698+imdhiep@users.noreply.github.com>
2024-05-10 13:59:36 +0700

    Add files via upload

---
+++ Document Scanner.py
@@ -0,0 +1,113 @@
+import cv2
+import numpy as np
+
+width = 540
+height = 640
+
+cap = cv2.VideoCapture(0)
+cap.set(3, width)
+cap.set(4, height)
+cap.set(10, 100)
+
+def stackImages(scale,imgArray):
+    rows = len(imgArray)
+    cols = len(imgArray[0])
+    rowsAvailable = isinstance(imgArray[0], list)
+    width = imgArray[0][0].shape[1]
+    height = imgArray[0][0].shape[0]
+    if rowsAvailable:
+        for x in range ( 0, rows):
+            for y in range(0, cols):
+                if imgArray[x][y].shape[:2] == imgArray[0][0].shape [:2]:
+                    imgArray[x][y] = cv2.resize(imgArray[x][y], (0, 0), None, scale, scale)
+                else:
+                    imgArray[x][y] = cv2.resize(imgArray[x][y], (imgArray[0][0].shape[1], imgArray[0][0].shape[0]), None, scale, scale)
+                if len(imgArray[x][y].shape) == 2: imgArray[x][y]= cv2.cvtColor( imgArray[x][y], cv2.COLOR_GRAY2BGR)
+        imageBlank = np.zeros((height, width, 3), np.uint8)
+        hor = [imageBlank]*rows
+        hor_con = [imageBlank]*rows
+        for x in range(0, rows):
+            hor[x] = np.hstack(imgArray[x])
+        ver = np.vstack(hor)
+    else:
+        for x in range(0, rows):
+            if imgArray[x].shape[:2] == imgArray[0].shape[:2]:
+                imgArray[x] = cv2.resize(imgArray[x], (0, 0), None, scale, scale)
+            else:
+                imgArray[x] = cv2.resize(imgArray[x], (imgArray[0].shape[1], imgArray[0].shape[0]), None,scale, scale)
+            if len(imgArray[x].shape) == 2: imgArray[x] = cv2.cvtColor(imgArray[x], cv2.COLOR_GRAY2BGR)
+        hor= np.hstack(imgArray)
+        ver = hor
+    return ver
+
+def preProcessing(img):
+    imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    imgBlur = cv2.GaussianBlur(imgGray, (5, 5), 1)
+    imgCanny = cv2.Canny(imgBlur, 100, 100)
+    imgDilate = cv2.dilate(imgCanny, np.ones((5, 5)), iterations=2)
+    imgThresh = cv2.erode(imgDilate, np.ones((5, 5)), iterations=1)
+    return imgThresh
+
+def getContours(img):
+    biggest = np.array([])
+    maxArea = 0
+    contours, hierarchy = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    for cnt in contours:
+        area = cv2.contourArea(cnt)
+        if area > 5000:
+            peri = cv2.arcLength(cnt, True)
+            approx = cv2.approxPolyDP(cnt, 0.02*peri, True)
+            if area > maxArea and len(approx) == 4:
+                biggest = approx
+                maxArea = area
+    cv2.drawContours(imgContour, biggest, -1, (255, 0, 0), 20)
+    return biggest
+
+def reorder(myPoints):
+    myPoints = myPoints.reshape((4,2))
+    reorderPoints = np.zeros((4,1,2), np.int32)
+    
+    add = np.sum(myPoints, axis=1)
+    reorderPoints[0] = myPoints[np.argmin(add)]
+    reorderPoints[3] = myPoints[np.argmax(add)]
+    
+    diff = np.diff(myPoints, axis=1)
+    reorderPoints[1] = myPoints[np.argmin(diff)]
+    reorderPoints[2] = myPoints[np.argmax(diff)]
+    
+    return reorderPoints
+
+def getWarp(img, biggest):
+    biggest = reorder(biggest)
+    pts1 = np.float32(biggest)
+    pts2 = np.float32([[0,0],[width,0],[0,height],[width,height]])
+    matrix = cv2.getPerspectiveTransform(pts1, pts2)
+    imgOutput = cv2.warpPerspective(img, matrix, (width,height))
+    imgCropped = imgOutput[10:imgOutput.shape[0]-10, 10:imgOutput.shape[1]-10]  
+    imgCropped = cv2.resize(imgCropped, (width,height))
+    return imgCropped
+
+while True:
+    susscess, img = cap.read()
+    img = cv2.resize(img, (width,height))
+    imgContour = img.copy()
+    imgThresh = preProcessing(img)
+    biggest = getContours(imgThresh)
+    
+    if biggest.size != 0:
+        imgWarped = getWarp(img, biggest)
+        imgArray = ([img, imgThresh],
+                    [imgContour, imgWarped])  
+        cv2.imshow('result', imgWarped)
+    else:
+        imgArray = ([img, imgThresh],
+                    [img, img]) 
+    
+    stackedImage = stackImages(0.6, imgArray)
+    cv2.imshow("work flow", stackedImage)
+    
+    
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+    
+    
